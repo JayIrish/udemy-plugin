8b3dcbc32c6296ea7a3001f91b779e2de13cc9d8
Jason <61055622+JayIrish@users.noreply.github.com>
2023-05-09 14:50:39 -0400

    Lesson 64

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// VARIABLES
+
+
+// INCULDES
+include(get_theme_file_path("/includes/front/enqueue.php"));
+include(get_theme_file_path("/includes/front/head.php"));
+
+// HOOKS
+add_action("wp_enqueue_scripts", "u_enqueue");
+add_action("wp_head", "u_head", 5);
